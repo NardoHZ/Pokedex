294611ac84c7caf4a95f99558fc0c8c4da45f31a
NardoHZ <48166282+NardoHZ@users.noreply.github.com>
2021-07-12 22:22:57 -0300

    .

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class = "column is-half is-offset-one-quarter">
+      <div v-for="(poke,index) in pokemons" :key="index">
+      <Pokemons :name="poke.name" :url="poke.url" :num="index + 1"/>
+    </div>
+
+  </div>
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Pokemons from './components/Pokemons.vue'
+
+export default {
+  name: 'App',
+  data (){
+    return {
+      pokemons: []
+    }
+  },
+  created : function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"). then (res => {
+      this.pokemons = res.data.results;
+    })
+  },
+  components:{
+    Pokemons
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
